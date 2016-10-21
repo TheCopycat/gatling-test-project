497ad7f4836f1ae41151311f6d21608211d4dd8f
Thibaut Robert <thibaut.robert@worldline.com>
2016-10-21 18:23:00 +0200

    add exo7: config

---
+++ gatling-test-project-simulation/src/test/scala/training/Exo7Config.scala
@@ -0,0 +1,58 @@
+package training
+
+import java.util.concurrent.TimeUnit
+
+import com.typesafe.config.ConfigFactory
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+import scala.concurrent.duration._
+
+
+class Exo7Config extends Simulation {
+  val conf = ConfigFactory.load()
+
+  val baseUrl = conf.getString("simulation.baseUrl")
+  val peakDuration = conf.getDuration("simulation.peakDuration", TimeUnit.SECONDS)
+  val peakUsers = conf.getInt("simulation.peakUsers")
+
+  val httpProtocol = http.baseURL(baseUrl)
+
+  val scn = scenario("Exo3")
+    // Repeat 5 times
+    .repeat(5)(
+      //Go to main page
+      exec(http("homepage").get("/shopping/main.xhtml"))
+      // Pause
+      .pause(2,3)
+      // Go to category page
+      .exec(http("category").get("/shopping/showproducts.xhtml?categoryName=Reptiles"))
+        // Pause
+      .pause(1)
+        //repeat 3 times
+      .repeat(3)(
+        // Go to product page
+        exec(http("product").get("/shopping/showitems.xhtml?productId=1011"))
+        // Pause
+        .pause(1)
+        // Go to item page
+        .exec(http("item").get("/shopping/showitem.xhtml?itemId=1011"))
+        // Pause
+        .pause(1)
+      )
+    )
+
+  setUp(scn
+    .inject(
+      atOnceUsers(1),
+      nothingFor(10.seconds),
+      rampUsers(10) over 10.seconds,
+      heavisideUsers(peakUsers) over peakDuration.seconds)
+    .protocols(httpProtocol)
+  ).assertions(
+    global.failedRequests.count.is(0),
+    forAll.requestsPerSec.greaterThan(5),
+    global.responseTime.percentile1.lessThan(200)
+  )
+
+}
